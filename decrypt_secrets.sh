719106ec434893e44fe6f7498cff58a7bdd76051
David Hettler <d.hettler@reply.de>
2021-02-04 22:22:12 +0100

    Various automation improvements

--- decrypt_secrets.sh
+++ decrypt_secrets.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 find . -name "*.env" -exec ansible-vault decrypt --vault-password-file=vault_secret {} \;
+find . -name "*.env" -exec chmod g+rw {} \;
--- encrypt_secrets.sh
+++ encrypt_secrets.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 find . -name "*.env" -exec ansible-vault encrypt --vault-password-file=vault_secret {} \;
+find . -name "*.env" -exec chmod g+rw {} \;
